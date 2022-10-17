a274472885c75b695936eb95850db640319b8852
saintabomination <saint.abomination@protonmail.com>
2022-10-17 12:00:31 +0200

    Adding example content

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 const App = (): JSX.Element =>
   (
     <>
-      <h1>Initial Commit</h1>
+      <h1>John Doe</h1>
+      <h2>Personal info</h2>
+      <ul>
+        <li>Age: 21</li>
+        <li>Country: Poland</li>
+        <li>Profession: Web Developer</li>
+      </ul>
+      <h2>Titles</h2>
+      <ul>
+        <li>Title 1</li>
+        <li>Title 2</li>
+        <li>Title 3</li>
+      </ul>
+      <h2>Contacts</h2>
+      <ul>
+        <li>Instagram</li>
+        <li>LinkedIn</li>
+        <li>Twitter</li>
+      </ul>
     </>
   );
 
