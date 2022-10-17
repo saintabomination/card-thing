fad809e4952cabb9161b2650c02577a99e227118
saintabomination <saint.abomination@protonmail.com>
2022-10-17 14:28:44 +0200

    Adding and setting up Express

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello, world!');
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+
+const DEFAULT_PORT: number = 3001;
+const port = process.env.PORT || DEFAULT_PORT;
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}.`);
+});
